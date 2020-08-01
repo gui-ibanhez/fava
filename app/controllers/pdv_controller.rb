fe763adef1563a7788e7d7d335db3d02c3ebd5cc
Guilherme Ibanhez <guilherme.ibanhez@gmail.com>
2020-08-01 10:11:14 -0300

    Corrected a bunch of bugs

--- app/controllers/pdv_controller.rb
+++ app/controllers/pdv_controller.rb
@@ -1,6 +1,9 @@
 class PdvController < ApplicationController
   protect_from_forgery with: :null_session
   def index
+    @customers = Customer.select(:id, :name).order(:name).map { |e| [e.name, e.id]})
+    @origins = Origin.select(:id, :name).order(:name).map { |e| [e.name, e.id]}
+    @products = Product.select(:nome, :id).order(:name).map { |e| [e.nome, e.id]}
   end
 
   def get_products
